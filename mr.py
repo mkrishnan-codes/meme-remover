0b4ecf8e4008265406bea13061fa31c63ee3b8e9
mkrishnan-codes <manu.krishnan@tarams.com>
2020-05-09 13:44:36 +0530

    logs added

--- mr.py
+++ mr.py
@@ -9,16 +9,25 @@ directory = '/Users/manu/Documents/whatsapp/'
 dest_folder = '/Users/manu/Documents/whatsapp/memes-separated'
 pytesseract.pytesseract.tesseract_cmd = r'/usr/local/bin/tesseract'
 # os.makedirs(dest_folder)
+count = 0
+moved = 0
 for filename in os.listdir(directory):
-
+    count = count+1
+    print(count)
+    print('#-----processing '+filename+' --------#')
     if (filename.endswith(".jpg") | filename.endswith(".png")):
-        currText = pytesseract.image_to_string(Image.open(directory+filename)
-        if "".__eq__(currText):
-            print('No meme')
-            # print(pytesseract.image_to_string(Image.open(directory+filename)))
-            # print(filename)
-        # shutil.move(directory+filename, dest_folder)
+        currText = pytesseract.image_to_string(Image.open(directory+filename))
+        if currText == "":
+            print(' : No meme in ' + filename)
+        else:
+            moved = moved+1
+            shutil.move(directory+filename, dest_folder)
+            print(str(moved) + ' files moved -'+filename)
         continue
     else:
-        print(filename)
+        print(filename, 'Not an image')
         continue
+unmoved = count-moved
+print('Total Processed = '+str(count))
+print('Total moved = '+str(moved))
+print('Unmoved = '+str(unmoved))
