8868c3b932adc695ee491c6225b1e020dc165d36
mkrishnan-codes <manu.krishnan@tarams.com>
2020-05-09 11:39:31 +0530

    file move test

--- mr.py
+++ mr.py
@@ -4,13 +4,18 @@ except ImportError:
     import Image
 import pytesseract
 import os
-directory = './'
+import shutil
+directory = './samples/'
+dest_folder = './samples/separated/'
 pytesseract.pytesseract.tesseract_cmd = r'/usr/local/bin/tesseract'
-
+os.makedirs(dest_folder)
 for filename in os.listdir(directory):
-    if filename.endswith(".jpg"):
+
+    if (filename.endswith(".jpg") | filename.endswith(".png")):
         print(pytesseract.image_to_string(Image.open(filename)))
+        print(filename)
+        shutil.move(directory+filename, dest_folder)
         continue
     else:
+        print(filename)
         continue
-
